e8f4d4199533fce432409c030cb45cf91fca491a
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-12-12 13:44:53 -0300

    Atualiza vizualicação de enunciado de exercícios de variáveis com JavaScript

--- variaveis/index.js
+++ variaveis/index.js
@@ -1,15 +1,7 @@
-/** Exercícios com variáveis
- * 1) Crie 3 variáveis contendo:
- * - Seu nome;
- * - Sua idade;
- * - Comida favorita;
- * 
- * Após declarar as variáveis e atribuir os valores, utilize o **`console.log`** para formar uma frase como:
- * “Meu nome é …, tenho …. anos e minha comida favorita é …..”
-*/
-
+// Inicialização de variáveis
 const nome = prompt("Qual é o seu nome?")
 const idade = prompt("Qual é a sua idade?")
 const comidaFavorita = prompt("Qual seria sua comida favorita?")
 
+// Visualização de variáveis inicializadas
 console.log(`Meu nome é ${nome}, tenho ${idade} anos e minha comida favorita é ${comidaFavorita}`)
