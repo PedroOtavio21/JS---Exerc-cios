4d3cd5d12e8c7eccf2561e82f1b89665410e26a2
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-12-12 13:22:33 -0300

    Adiciona primeiro commit de repositório de exercícios, com o primeiro diretório de exercícios: variaveis/index.js

---
+++ variaveis/index.js
@@ -0,0 +1,15 @@
+/** Exercícios com variáveis
+ * 1) Crie 3 variáveis contendo:
+ * - Seu nome;
+ * - Sua idade;
+ * - Comida favorita;
+ * 
+ * Após declarar as variáveis e atribuir os valores, utilize o **`console.log`** para formar uma frase como:
+ * “Meu nome é …, tenho …. anos e minha comida favorita é …..”
+*/
+
+const nome = prompt("Qual é o seu nome?")
+const idade = prompt("Qual é a sua idade?")
+const comidaFavorita = prompt("Qual seria sua comida favorita?")
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e minha comida favorita é ${comidaFavorita}`)
